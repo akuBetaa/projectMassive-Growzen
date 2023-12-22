cca5f0af590a8803061ce1ba84b6cc133adab122
Beta Nurul Awwalin <beta.n.awwalin@gmail.com>
2023-12-22 17:51:27 +0700

    update login

--- backend-DOM/controllers/Auth.js
+++ backend-DOM/controllers/Auth.js
@@ -1,93 +1,73 @@
 import argon2 from "argon2";
 import db from "../config/Database.js";
 
+
+//untuk login
 export const Login = async (req, res) => {
   try {
-    const q = "SELECT * FROM user WHERE email = ?";
-    const [rows] = await db.execute(q, [req.body.email]);
+    const email = req.body.email;
 
-    if (rows.length === 0) {
-      return res.status(404).json({ msg: "Email user tidak ditemukan" });
-    }
+    // Gantilah sesuai dengan format kolom dan tabel di database Anda
+    const query = 'SELECT * FROM user WHERE email = ? LIMIT 1';
 
-    const user = rows[0];
+    db.query(query, [email], async (error, results) => {
+      if (error) {
+        console.error(error);
+        throw new Error("Terjadi kesalahan server");
+      }
 
-    // Pastikan user memiliki properti passwordHash
-    if (!user.passwordHash) {
-      return res.status(500).json({ msg: "Password hash tidak valid" });
-    }
+      const user = results[0];
 
-    const passwordMatch = await argon2.verify(user.passwordHash, req.body.password);
+      if (!user)
+        throw new Error("Email user tidak ditemukan");
 
-    if (passwordMatch) {
-      // req.session.userId = user.id; // Pastikan properti ID pengguna yang sesuai
-      // return res.status(200).json({ msg: "Login berhasil" });
+      const match = await argon2.verify(user.password, req.body.password);
+      if (!match)
+        throw new Error("Password yang dimasukkan salah");
+
       req.session.userId = user.uuid;
       const { uuid, name, email, role } = user;
+
       res.status(200).json({ uuid, name, email, role });
-    } else {
-      return res.status(400).json({ msg: "Password yang dimasukkan salah" });
-    }
+    });
   } catch (error) {
-    console.error("Kesalahan selama proses login:", error);
+    console.error("Kesalahan selama proses login:", error.message);
     return res.status(500).json({ msg: "Kesalahan server internal" });
   }
 };
 
-// export const Me = async (req, res) => {
-//   try {
-//     // Pastikan pengguna sudah login
-//     if (!req.session.userId) {
-//       return res.status(401).json({ msg: 'Mohon login ke akun Anda' });
-//     }
-
-//     // Log the value of req.session.userId for debugging
-//     console.log('User ID from session:', req.session.userId);
-
-//     // Ambil informasi pengguna berdasarkan ID sesi
-//     const q = "SELECT * FROM user WHERE id = ?";
-//     const [rows] = await db.execute(q, [req.session.userId]);
-
-//     // Pastikan pengguna ditemukan
-//     if (rows.length === 0) {
-//       return res.status(404).json({ msg: 'User tidak ditemukan' });
-//     }
-
-//     // Kirim informasi pengguna sebagai respons
-//     const user = rows[0];
-//     res.status(200).json(user);
-
-//   } catch (error) {
-//     console.error("Kesalahan selama proses mendapatkan informasi pengguna:", error);
-//     return res.status(500).json({ msg: "Kesalahan server internal" });
-//   }
-// };
 
 export const Me = async (req, res) => {
-  try {
-    // Pastikan pengguna sudah login
-    if (!req.session.userId) {
-      return res.status(401).json({ msg: 'Mohon login ke akun Anda' });
-    }
-
-    // Log the value of req.session.userId for debugging
-    console.log('User ID from session:', req.session.userId);
-
-    // Ambil informasi pengguna berdasarkan UUID sesi
-    const q = "SELECT * FROM user WHERE uuid = ?";
-    const [rows] = await db.execute(q, [req.session.userId]);
-
-    // Pastikan pengguna ditemukan
-    if (rows.length === 0) {
-      return res.status(404).json({ msg: 'User tidak ditemukan' });
-    }
-
-    // Kirim informasi pengguna sebagai respons
-    const user = rows[0];
-    res.status(200).json(user);
-
-  } catch (error) {
-    console.error("Kesalahan selama proses mendapatkan informasi pengguna:", error);
-    return res.status(500).json({ msg: "Kesalahan server internal" });
+  if(!req.session.userId) {
+      return res.status(401).json({ msg : "Mohon login ke akun Anda"})
   }
-};
+
+  const query = 'SELECT uuid, name, email, role FROM user   WHERE uuid = ?';
+
+  db.query(query, [req.session.userId], (err, results) => {
+      if (err) {
+          console.error('Error executing query:', err);
+          return res.status(500).json({ msg: 'Error while fetching user data' });
+      }
+
+      if (results.length === 0) {
+          return res.status(404).json({ msg: 'Email user tidak ditemukan' });
+      }
+
+      const user = results[0];
+      res.status(200).json(user);
+  });
+}
+
+export const Logout = (req, res) => {
+  // if (!req.session.userId) {
+  //   return res.status(401).json({ msg : "Mphpn login ke akun Anda"})
+  // }
+
+  // const query = "SELECT uuid, name, email, role FROM user   WHERE uuid = ?"
+  req.session.destroy((err) => {
+    if(err)
+        return res.status(400).json({ msg : "Tidak dapat logout"});
+    res.status(200).json({ msg : "Anda telah logout"});
+})
+}
--- backend-DOM/routes/AuthRoute.js
+++ backend-DOM/routes/AuthRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { Login, Me } from "../controllers/Auth.js"
+import { Login, Me, Logout} from "../controllers/Auth.js"
 
 const router = express.Router();
 
 router.post('/login', Login);
 router.get('/me', Me);
+router.delete('/logout', Logout);
 
 export default router;
